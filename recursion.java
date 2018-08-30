ba4f686e38627b1f77a7e8caa02cca31eb1129c5
Adam <micro.83@hotmail.com>
2018-08-30 15:30:10 +0300

    Add factorial number

--- recursion.java
+++ recursion.java
@@ -5,7 +5,8 @@ import java.util.*;
 import java.lang.*;
 
 class Rextester
-{  
+{   
+    // The power of number by recursion
     public static int power(int n, int p){
        
         if (p == 0){
@@ -13,8 +14,22 @@ class Rextester
         }
         return n * power(n,p-1);
     }
+    
+    // Factorial number by recursion
+    public static int factorial(int n){
+       
+        if (n == 0){
+           return 1; 
+        }
+        return n*factorial(n-1);
+    }
+    
+    
+    
     public static void main(String args[])
     {
         System.out.println(power(3,1));
+        System.out.println(factorial(5));
+        
     }
 }

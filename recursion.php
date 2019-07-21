61c66a61273f84b0e3af9afada039ab03588d5fc
Adam <micro.83@hotmail.com>
2019-07-21 11:46:52 +0300

    Update recursion.php

--- recursion.php
+++ recursion.php
@@ -1,27 +1,24 @@
 <?php //php 7.0.8
 
- // The power of number by recursion
-function power($n, $p){
-       
-        if ($p == 0){
-           return 1; 
-        }
-        return $n * power($n,$p-1);
-    }
-    
+// The power of number by recursion
+function power($n, $p)
+{
+  if ($p == 0) {
+      return 1;
+  }
+  return $n * power($n,$p-1);
+}
+  
 // Factorial number by recursion
-function factorial($n){
-       
-        if ($n == 0){
-           return 1; 
-        }
-        return $n*factorial($n-1);
-    }
+function factorial($n)
+{
+  if ($n == 0) {
+      return 1; 
+  }
+  return $n*factorial($n-1);
+}
 
+echo power(5,2)."\n";
+echo factorial(5)."\n";
 
-
-
-  echo power(5,2)."\n";
-  echo factorial(5)."\n";
-    
-?>
+  

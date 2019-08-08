ec80b5af482a59c68975e4fbe68de2bb09598f02
Adam <micro.83@hotmail.com>
2019-08-08 16:49:24 +0300

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -4,24 +4,22 @@
 using namespace std;
 
 // The power of number by recursion
-int power(int n, int p){
-       
-        if (p == 0){
-           return 1; 
-        }
-        return n * power(n,p-1);
+int power(int n, int p) 
+{       
+    if (p == 0) {
+        return 1; 
     }
+    return n * power(n,p-1);
+}
     
 // Factorial number by recursion
-int factorial(int n){
-       
-        if (n == 0){
-           return 1; 
-        }
-        return n*factorial(n-1);
+int factorial(int n)
+{
+    if (n == 0) {
+        return 1; 
     }
-
-
+    return n*factorial(n-1);
+}
 
 int main()
 {
